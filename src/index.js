cf78f9faff3314fdb785243a1bf1ec7570224663
Yu Sangyoon <ysy970923@gmail.com>
2020-03-29 23:56:46 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Result = require('./models/result')
 const userRouter = require('./routers/user')
 const resultRouter = require('./routers/result')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
