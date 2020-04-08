a35e9e0b8bc5a5ce179a0d2ad0747823b268c898
Yu Sangyoon <ysy970923@gmail.com>
2020-04-08 21:37:50 +0900

    login changed

--- src/routers/user.js
+++ src/routers/user.js
@@ -17,7 +17,7 @@ router.post('/users', async (req, res) => {
 
 router.post('/users/login', async (req, res) => {
   try {
-    const user = await User.findByCredentials(req.body.email, req.body.password)
+    const user = await User.findByCredentials(req.body.uid)
     const token = await user.generateAuthToken()
     res.send({ user: user, token })
   } catch (e) {
