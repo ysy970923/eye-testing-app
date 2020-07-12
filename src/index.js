00632aad3eeb01414102719155f098fe897dc067
Yu Sangyoon <ysy970923@gmail.com>
2020-07-12 14:42:30 +0900

    points for toy-proto has been added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const resultRouter = require('./routers/result')
+const pointRouter = require('./routers/point')
 
 const app = express()
 const port = process.env.PORT
@@ -9,6 +10,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 app.use(resultRouter)
+app.use(pointRouter)
 
 app.listen(port, () => {
   console.log('Server is up on port', port)
